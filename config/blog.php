c653aca12712aabb4d9f9546283f25c599356671
cmershen1 <sry2011211535@bupt.edu.cn>
2016-04-16 09:57:15 +0800

    first commit

---
+++ config/blog.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @Author: cmershen
+ * @Date:   2016-04-12 09:12:55
+ * @Last Modified by:   anchen
+ * @Last Modified time: 2016-04-12 15:17:30
+ */
+return [
+    'name' => "Cmershen's blog",
+    'title' => "Cmershen的博客，仅供练手",
+    'subtitle' => 'http://blog.cmershen.com',
+    'description' => 'Cmershen的博客，仅供练手',
+    'author' => 'Cmershen',
+    'page_image' => 'home_green.jpg',
+    'posts_per_page' => 10,
+    'uploads' => [
+        'storage' => 'local',
+        'webpath' => '/uploads/',
+    ],
+];
